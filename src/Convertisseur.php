48f72fe4b021892ce9954e6dcdf9210e1240e848
darkaine <duretkevin@live.fr>
2023-04-11 13:32:55 +0200

    Premier commit

---
+++ src/Convertisseur.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace src;
+
+class Convertisseur
+{
+    private $tauxChangeDollarEnEuro = 1.16;
+    private $montantTransactionMinimum = 100;
+    private $prixTransaction = 1;
+
+    public function convertirDollarsEnEuro(array $listDeDollars): array
+    {
+        $listEnEuro = [];
+
+        foreach($listDeDollars as $dollar) {
+            $listEnEuro[] = $dollar / $this->tauxChangeDollarEnEuro;
+        }
+
+        return $listEnEuro;
+    }
+
+    public function calculPrixTransaction(float $valeurEuro): bool|float
+    {
+        if($valeurEuro >= $this->montantTransactionMinimum) {
+            return false;
+        }
+        return $this->prixTransaction;
+    }
+
+    public function encaissementEuro(array $listEnDollars): int
+    {
+        $listEnEuro = $this->convertirDollarsEnEuro($listEnDollars);
+        $encaissementEuro = 0;
+
+        foreach($listEnEuro as $valeurEuro) {
+            $encaissementEuroValeur = $this->calculPrixTransaction($valeurEuro);
+            if($encaissementEuroValeur) {
+                $encaissementEuro -= $encaissementEuroValeur;
+            }
+            $encaissementEuro += $valeurEuro;
+        }
+
+        return $encaissementEuro;
+    }
+}
---
+++ tests/EncaissementEuroTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+use src\Convertisseur;
+
+require("./src/Convertisseur.php");
+
+class EncaissementEuroTest extends TestCase
+{
+    public function testConvertirEnUnEuro(): void
+    {
+        $convertisseur = new Convertisseur();
+        $this->assertEquals([1], $convertisseur->convertirDollarsEnEuro([1.16]));
+    }
+
+    public function testEncaissementEuroPourUneTransactionDePlusDeCentEuro(): void
+    {
+        $convertisseur = new Convertisseur();
+        $this->assertEquals(false, $convertisseur->calculPrixTransaction(116));
+    }
+}
